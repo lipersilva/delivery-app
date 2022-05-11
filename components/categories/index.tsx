f411e5fd4352e20fd8bb7b5e09422404b5cc4d8f
Filipe Rodrigues <lipe_rsilva@hotmail.com>
2022-05-11 11:43:22 -0300

    feat: alterado categorias

--- components/categories/index.tsx
+++ components/categories/index.tsx
@@ -7,26 +7,41 @@ export const Categories = () => {
   const categories = [
     {
       name: "Popular",
-      legend: "+286 opções",
+      legend: "286+ opções",
       iconName: "fire",
     },
     {
       name: "Delivery",
-      legend: "+1843 opções",
-      iconName: "bike",
+      legend: "1843+ opções",
+      iconName: "motorcycle",
     },
     {
       name: "Alta Classe",
-      legend: "+25 opções",
+      legend: "25+ opções",
       iconName: "wallet",
+    },
+    {
+      name: "Jantar em ",
+      legend: "182+ opções",
+      iconName: "knife",
+    },
+    {
+      name: "Retirar em",
+      legend: "3548+ opções",
+      iconName: "store",
+    },
+    {
+      name: "Mais próximo",
+      legend: "44+ opções",
+      iconName: "map",
     },
   ];
   return (
     <div className={Styles.header}>
       <div className="flex">
-        <h2 className="grow">Explorar Categorias</h2>
-        <Link href="./ctegories" >
-          <a>Ver todas</a>
+        <h2 className="grow font-bold mb-8">Explorar Categorias</h2>
+        <Link href="./categories" >
+          <a className="font-bold">Ver todas</a>
         </Link>
       </div>
       <div className="flex gap-8">
