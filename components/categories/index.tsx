b8c058f3ef3f1ed76aab3842a865b0fe53e1d4fb
Filipe Rodrigues <lipe_rsilva@hotmail.com>
2022-05-11 16:13:44 -0300

    feat: adicionado novas categorias, cores e alinhamento

--- components/categories/index.tsx
+++ components/categories/index.tsx
@@ -9,42 +9,48 @@ export const Categories = () => {
       name: "Popular",
       legend: "286+ opções",
       iconName: "fire",
+      backgroundSection:"#FB6D3A"
     },
     {
       name: "Delivery",
       legend: "1843+ opções",
       iconName: "motorcycle",
+      backgroundSection:"#503E9D"
     },
     {
       name: "Alta Classe",
       legend: "25+ opções",
       iconName: "wallet",
+      backgroundSection:"#FACD5D"
     },
     {
-      name: "Jantar em ",
+      name: "Jantar",
       legend: "182+ opções",
       iconName: "knife",
+      backgroundSection:"#FB6D3A"
     },
     {
       name: "Retirar em",
       legend: "3548+ opções",
       iconName: "store",
+      backgroundSection:"#503E9D"
     },
-    {
-      name: "Mais próximo",
-      legend: "44+ opções",
-      iconName: "map",
-    },
+    // {
+    //   name: "Mais próximos",
+    //   legend: "44+ opções",
+    //   iconName: "map",
+    //   backgroundSection:"#FACD5D"
+    // },
   ];
   return (
-    <div className={Styles.header}>
+    <div>
       <div className="flex">
         <h2 className="grow font-bold mb-8">Explorar Categorias</h2>
         <Link href="./categories" >
           <a className="font-bold">Ver todas</a>
         </Link>
       </div>
-      <div className="flex gap-8">
+      <div className="flex gap-6 justify-between">
         {categories.map((category)=>(
           <Category key={category.name} {...category} /> 
         ))}
--- components/category/index.tsx
+++ components/category/index.tsx
@@ -1,17 +1,27 @@
 import Styles from './styles.module.css'
+import { IconBase } from '../../components'
+import Link from 'next/link';
 
 type Props = {
   name: string;
   iconName: string;
   legend: string;
+  backgroundSection:string;
 }
 
-export const Category = ( { name, iconName, legend}: Props) => {
+export const Category = ( { name, iconName, legend, backgroundSection}: Props) => {
   return(
-    <div className={Styles.card}>
-      <div>{iconName}</div>
+    <Link href="/categoria">
+      <a className={Styles.card}>
+      <div 
+        className={Styles.icon} 
+        style={{backgroundColor: backgroundSection}}
+      >
+        <IconBase iconName={iconName} color="white" size={48}/>
+      </div>
       <h3>{name}</h3>
       <p>{legend}</p>
-    </div>
+    </a>
+    </Link>
   );
 }
