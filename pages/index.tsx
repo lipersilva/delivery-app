5e0460bc9ecd0cfb4e3b816da7f73bf698f63abd
Filipe Rodrigues <lipe_rsilva@hotmail.com>
2022-05-14 13:12:28 -0300

    feat: adicionado importação do componente pratos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,18 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Link from 'next/link'
-import { Categories, Highlights, Navbar, PageHead, Sidebar } from '../components'
+import { Categories, FoodsList, Highlights, Navbar, PageHead, Sidebar } from '../components'
 
 
 const Home: NextPage = () => {
   return (
     <>
       <PageHead title="Home" description="Sistema de Delivery" keywords="delivery, food"/>
-
-
       <div className= "container m-auto flex">
         <Sidebar/>
-        <div className= "grow p-8 " >
+        <div className= "grow p-8" >
           <Navbar/>
           <Categories/>
           <Highlights/>
+          <FoodsList/>
         </div>
       </div>
     </>
