c2de9202b314022be0d09292eed4e94e7be0cc05
Filipe Rodrigues <lipe_rsilva@hotmail.com>
2022-05-14 16:01:46 -0300

    feat: adicionado página restaurantes

---
+++ pages/restaurant.tsx
@@ -0,0 +1,43 @@
+import { NextPage } from "next";
+import { DetailsItem, FoodsList, Navbar, PageHead, Sidebar } from "../components";
+import Styles from './restaurant.module.css'
+
+const Restaurant:NextPage = () => {
+  return (
+    <>
+      <PageHead title="Restaurante - Burguer King" description="Sistema de Delivery" keywords="delivery, food"/>
+      <div className= "container m-auto flex">
+        <Sidebar/>
+        <div className= "grow p-8" >
+          <Navbar/>
+          <div>[imagem]</div>
+          <div className={Styles.header}>
+            <div className={Styles.details}>
+              <h1>Burguer King</h1>
+              <p>It is one of the most iconic and well-recognizable fast food restaurants out there which offers really amazing food and drinks.</p>
+            </div>
+            <div className={Styles.tags}>
+              <div>
+                <span>Entrega Grátis</span>
+                <span>Compre 1 leve 2</span>
+                <span>Abre às 12h</span>
+              </div>
+            </div>
+          </div>
+          <div className="mb-12">
+            <DetailsItem 
+              rate={4.5} 
+              reviews={1.873} 
+              category="Burguer" 
+              price={1} 
+              distance="4,3 km (Irving St, San Francisco, California)"
+            /> 
+          </div>
+          <FoodsList/>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Restaurant;
