261b985ca56e0d757e5b0a34cd320b3afaac038c
Filipe Rodrigues <lipe_rsilva@hotmail.com>
2022-05-16 15:19:07 -0300

    feat: adicionado arquivo types para components

---
+++ components/food-item/types.ts
@@ -0,0 +1,10 @@
+export type TypePageProps = {
+  id: string;
+  name: string,
+  imageUrl: string;
+  reviews?: number | undefined;
+  deliveryFee: string,
+  rate: number,
+  category: string;
+  deliveryTime: string,
+};
